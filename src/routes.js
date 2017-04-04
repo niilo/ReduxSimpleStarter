2ae1c01c3eba6719024011ab7ecc50cff81444a2
Niilo Ursin <niilo.ursin@gmail.com>
2017-04-05 00:15:33 +0300

    add route to posts/:id

--- src/routes.js
+++ src/routes.js
@@ -4,10 +4,12 @@ import { Switch, Route, IndexRoute } from 'react-router'
 import App from './components/app'
 import PostIndex from './components/post_index'
 import PostsNew from './components/posts_new'
+import PostsShow from './components/posts_show'
 
 export default (
     <Switch>
         <Route exact path="/" component={PostIndex} />
         <Route path="/posts/new" component={PostsNew} />
-    </Switch>
+        <Route path="/posts/:id" component={PostsShow} />
+     </Switch>
 )
