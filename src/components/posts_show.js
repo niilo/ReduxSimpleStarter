c5c48a6854e0e80bcdecf24467641d8166257c27
Niilo Ursin <niilo.ursin@gmail.com>
2017-04-05 23:28:39 +0300

    add lin to index

--- src/components/posts_show.js
+++ src/components/posts_show.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { fetchPost } from '../actions/index'
+import { Link } from 'react-router-dom'
 
 class PostsShow extends Component {
     componentWillMount() {
@@ -14,6 +15,7 @@ class PostsShow extends Component {
 
         return (
             <div>
+                <Link to="/">Back to index</Link>
                 <h3>{post.title}</h3>
                 <h6>Categories: {post.categories}</h6>
                 <p>{post.content}</p>
