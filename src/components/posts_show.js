0098f6e7ec9344e9257c67f759f9fa039085ad3b
Niilo Ursin <niilo.ursin@gmail.com>
2017-04-05 23:17:56 +0300

    add fetchPost

--- src/components/posts_show.js
+++ src/components/posts_show.js
@@ -1,9 +1,14 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { fetchPost } from '../actions/index'
 
 class PostsShow extends Component {
+    componentWillMount() {
+        this.props.fetchPost(this.props.match.params.id)
+    }
     render() {
         return <div>Show post {this.props.match.params.id}</div>
     }
 }
 
-export default PostsShow
+export default connect(null, { fetchPost })(PostsShow)
