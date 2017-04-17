d7fe16110fbdf3134a49f1a2c68cd6fb7ed50d75
Niilo Ursin <niilo.ursin@gmail.com>
2017-04-17 15:23:17 +0300

    redux

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,9 @@
+export default function() {
+    return [
+        { title: 'Javascript..', pages: 101},
+        { title: 'dadada', pages: 11},
+        { title: 'book3', pages: 412},
+        { title: 'kirja', pages: 32},
+        { title: 'Eloq', pages: 1},
+    ]
+}
